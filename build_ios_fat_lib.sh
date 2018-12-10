4c3dffa369a09bc195eda519d863523c6e940c9d
Matthew Iannucci <mpiannucci@gmail.com>
2018-12-09 21:52:32 -0500

    Cleanup and add fat binary creation

--- build_ios_fat_lib.sh
+++ build_ios_fat_lib.sh
@@ -1,2 +1,4 @@
 ~/Development/zig-master/build/bin/zig build-lib --target-os ios --target-arch x86_64 -mios-version-min 9.0 --static --name hello-zig-ios-x86_64 src/main.zig
 ~/Development/zig-master/build/bin/zig build-lib --target-os ios --target-arch aarch64v8 -mios-version-min 9.0 --static --name hello-zig-ios-aarch64 src/main.zig
+
+lipo -create hello-zig-ios-x86_64.a hello-zig-ios-aarch64.a -output hello-zig-ios-universal.a
